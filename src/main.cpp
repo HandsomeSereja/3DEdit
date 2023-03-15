5956cefd3f55bb06cec114d4c700f9b58830baa6
HandsomeSereja <sernek228@mail.ru>
2023-03-15 23:08:33 +0500

    Создал окно, и выключение его через ESCAPE

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,51 @@
 #include <iostream>
 
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* Window, int width, int height) 
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
-        return -1;
+	{
+		std::cout << "glfwInit ERROR" << std::endl;
+		return -1;
+	}
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello nigga", NULL, NULL);
+    window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "3DEdit", nullptr, nullptr);
     if (!window)
     {
+        std::cout << "Load window ERROR" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -28,9 +58,10 @@ int main(void)
 		return -1;
 	}
 	
-	std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 	
-	glClearColor(0, 1, 0, 1);
+	glClearColor(1, 0, 0, 1);
 	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
